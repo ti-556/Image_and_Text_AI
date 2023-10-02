8aadc9760f92eb4dc9e3f9e75eba59166f609d6c
TI <131604147+ti-556@users.noreply.github.com>
2023-10-02 19:42:47 +0900

    Update imagechatapp.py

--- imagechatapp.py
+++ imagechatapp.py
@@ -5,6 +5,7 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.lang import Builder
+from chat_gpt_api import chatgpt_api_call
 
 Builder.load_file("imagemenu.kv")
 
@@ -12,15 +13,15 @@ class GridLayout(GridLayout):
     def __init__(self, **kwargs):
         super(GridLayout, self).__init__(**kwargs)
         
-        self.cols = 3
-        self.rows = 3
+        self.imageselected = False
+        self.filepath = ""
+        
+        self.cols = 1
         
         self.input = Label(
             text = "user input: ",
             size_hint_y = None,
-            height = 300,
-            size_hint_x = None,
-            width = 500
+            height = 50,
             )
         self.add_widget(self.input)
         
@@ -28,19 +29,15 @@ class GridLayout(GridLayout):
             multiline = False,
             size_hint_y = None,
             height = 300,
-            size_hint_x = None,
-            width = 500
             )
         self.add_widget(self.textinput)
         
         self.submit = Button(
             text = "Submit", 
             background_color = (0.3, 1, 0.8, 1),
-            font_size = 20,
+            font_size = 30,
             size_hint_y = None,
-            height = 100,
-            size_hint_x = None,
-            width = 300
+            height = 50,
             )
         self.submit.bind(on_press=self.press)
         self.add_widget(self.submit)
@@ -48,28 +45,28 @@ class GridLayout(GridLayout):
         self.output = Label(
             text = "response: ",
             size_hint_y = None,
-            height = 300,
-            size_hint_x = None,
-            width = 500
+            height = 100,
         )
         self.add_widget(self.output)
         
         self.useroutput = Label(
             text = "",
-            size_hint_y = None,
+            text_size = (1500, None),
             height = 300,
-            size_hint_x = None,
-            width = 500
             )
         self.add_widget(self.useroutput)
         
     def press(self, instance):
         userinput = self.textinput.text
+        if self.imageselected:
+            self.useroutput.text = chatgpt_api_call(image_path = self.filepath, prompt = "")
+        if not self.imageselected:
+            self.useroutput.text = chatgpt_api_call(image_path = "", prompt = userinput)
         self.textinput.text = ""
-        self.useroutput.text = userinput
         
     def selected(self, filename):
-        self.useroutput.text = filename[0]
+        self.filepath = filename[0]
+        self.imageselected = True
         
 class Main(App):
     def build(self):
